f35142cd5eb9f517e007ca42944013fe30772811
Ryan Van Eck <ryan.vaneck@cove.is>
2024-09-05 12:44:21 -0500

    hat

--- App.js
+++ App.js
@@ -1,7 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { applicationId } from 'expo-application';
+import { useCallback, useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    console.log('Notification received');
+    return ({
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+      shouldShowAlert: true,
+    })
+  },
+});
+
+export const usePushNotifications = () => {
+
+  const test = useCallback(
+    async () => {
+
+      const { status } = await Notifications.getPermissionsAsync();
+      console.log({ status })
+      if (status !== 'granted') {
+        await Notifications.requestPermissionsAsync();
+      }
+      const pushTokenString = await Notifications.getDevicePushTokenAsync();
+      console.log({ pushTokenString })
+    },
+    [],
+  );
+
+  useEffect(() => {
+    test()
+  }, [test])
+
+};
 
 export default function App() {
+  usePushNotifications();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
